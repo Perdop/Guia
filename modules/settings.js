9c10ce51faaf2f31109421b1027a16cfae3f2798
Pedro <operdop@proton.me>
2024-10-08 16:41:41 -0300

    Add files via upload

--- modules/settings.js
+++ modules/settings.js
@@ -1,4 +1,8 @@
-export var searchHotKeys = ['D', 'G', 'B', 'Y']
-export var searchAdress = ['https://duckduckgo.com/', 'https://www.google.com/search?q=', 'https://www.bing.com/search?q=', 'https://yandex.com/search?text=']
+// Search
+export var searchHotKeys = ['D', 'G', 'B', 'Y'];
+export var searchAdress = ['https://duckduckgo.com/', 'https://www.google.com/search?q=', 'https://www.bing.com/search?q=', 'https://yandex.com/search?text='];
 
 export var defaultEngine = searchAdress[0];
+
+// Main
+export var defineColumns = 3;
--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-// search
+// Search / Chama a funcao buscar
 import * as search from './modules/search.js';
 
 var searchBar = document.getElementById('search');
 
 searchBar.addEventListener('keydown', search.searchFunction)
+
+// Main / Define o numero de colunas
+import * as settings from './modules/settings.js';
+var main = document.querySelector('main');
+main.style.gridTemplateColumns= `repeat(${settings.defineColumns}, auto)`;
+
+// Footer
+// Abre e fecha o botao de adicionar
+var newBtn = document.querySelector('#newBtn');
+var newBtnOptions = document.querySelector('#newBtnOptions');
+var newBtnCheck = document.querySelector('#newBtnCheck');
+
+function newBtnFunction() {
+    switch (newBtnCheck.checked) {
+        case false:
+            newBtnOptions.classList.add('show');
+            newBtnCheck.checked = true
+            break;
+        case true:
+            newBtnOptions.classList.remove('show');
+            newBtnCheck.checked = false
+            break;
+    }
+}
+
+newBtn.addEventListener('click', newBtnFunction); 
